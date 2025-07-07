835daf1ab130eeb7c964832d2528f39475bb2215
yj.jo <jojojo7827@naver.com>
2025-07-08 07:54:27 +0900

    Initial Commit

---
+++ RefrigeratorStorage/RefrigeratorStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefrigeratorStorageApp.swift
+//  RefrigeratorStorage
+//
+//  Created by QMIT_iOS on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RefrigeratorStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RefrigeratorStorageTests/RefrigeratorStorageTests.swift
@@ -0,0 +1,16 @@
+//
+//  RefrigeratorStorageTests.swift
+//  RefrigeratorStorageTests
+//
+//  Created by QMIT_iOS on 7/8/25.
+//
+
+import Testing
+
+struct RefrigeratorStorageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
